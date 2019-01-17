ad19fd48e3085fe59eae784a3531e7e78b605e98
韦月新 <1142149420@qq.com>
2019-01-18 01:54:17 +0800

    提交项目文件

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <paging 
+      :page-size="pagesize"
+      :page-count="pageCount"
+      :total.sync="total"
+      :current.sync="current"
+      @change="changePage"/>
+  </div>
+</template>
+
+<script>
+import paging from './components/paging.vue'
+
+export default {
+  name: 'app',
+  components: {
+    paging
+  },
+  data() {
+    return {
+      current: 1, // 当前页
+      open: false,
+      total:150, // 总记录数
+      pagesize: 15, // 一页多少条
+      pageCount: 7 // 页码按钮的数量，当总页数超过该值时会折叠
+    };
+  },
+  methods:{
+    changePage(e) {
+      console.log(e);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
